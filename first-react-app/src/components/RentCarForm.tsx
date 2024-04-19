9608239b88fa0423e44a6c9dabbfa3c1b020fde3
Thundem <vovkandrij7@gmail.com>
2024-04-19 23:43:31 +0300

    changed clientName to firstName and lastName

--- first-react-app/src/components/RentCarForm.tsx
+++ first-react-app/src/components/RentCarForm.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import dayjs, { Dayjs } from 'dayjs';
+import InputMask from 'react-input-mask';
 
-interface RentCar {
-    clientName: string;
+export interface RentCar {
+    firstName: string;
+    lastName: string;
     phoneNumber: string;
     email: string;
     placeOfIssue: string;
@@ -11,9 +13,16 @@ interface RentCar {
     comments: string;
 }
 
-const RentCarForm: React.FC = () => {
+interface RentCarFormProps {
+    form: RentCar;
+    setForm: React.Dispatch<React.SetStateAction<RentCar>>;
+    onSubmit: (e: React.FormEvent) => void;
+}
+
+const RentCarForm: React.FC<RentCarFormProps> = ({ form, setForm, onSubmit}) => {
     const initialFormState: RentCar = {
-        clientName: '',
+        firstName: '',
+        lastName: '',
         phoneNumber: '',
         email: '',
         placeOfIssue: '',
@@ -21,8 +30,6 @@ const RentCarForm: React.FC = () => {
         finishRentDate: dayjs(),
         comments: '',
     };
-
-    const [form, setForm] = useState<RentCar>(initialFormState);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
         setForm({ ...form, [e.target.name]: e.target.value });
@@ -42,20 +49,33 @@ const RentCarForm: React.FC = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit} id='rentCar'>
+        <form onSubmit={(e) => { handleSubmit(e); onSubmit(e); }} id='rentCar'>
+            <div id='fullName'>
             <input 
-                id='clientName' 
-                name="clientName" 
-                value={form.clientName} 
-                onChange={handleChange} 
-                placeholder="Client Name" 
-            />
-            <input 
+                    id='firstName' 
+                    name="firstName" 
+                    value={form.firstName} 
+                    onChange={handleChange} 
+                    placeholder="First Name" 
+                    required
+                />
+                <input 
+                    id='lastName' 
+                    name="lastName" 
+                    value={form.lastName} 
+                    onChange={handleChange} 
+                    placeholder="Last Name" 
+                    required
+                />
+            </div>
+            <InputMask 
+                mask="+38(099) 999 9999"
                 id='phoneNumber' 
                 name="phoneNumber" 
                 value={form.phoneNumber} 
                 onChange={handleChange} 
-                placeholder="Phone Number" 
+                placeholder="Phone Number"
+                required 
             />
             <input 
                 id='email' 
@@ -63,6 +83,7 @@ const RentCarForm: React.FC = () => {
                 value={form.email} 
                 onChange={handleChange} 
                 placeholder="Email" 
+                required
             />
             <input 
                 id='placeOfIssue' 
@@ -70,6 +91,7 @@ const RentCarForm: React.FC = () => {
                 value={form.placeOfIssue} 
                 onChange={handleChange} 
                 placeholder="Place of Issue" 
+                required
             />
             <input 
                 type="date" 
@@ -77,31 +99,36 @@ const RentCarForm: React.FC = () => {
                 name="startRentDate" 
                 value={form.startRentDate.format('YYYY-MM-DD')} 
                 onChange={handleDateChange('startRentDate')} 
+                required
             />
             <input 
                 type="date" 
                 id="finishRentDate" 
                 name="finishRentDate" 
                 value={form.finishRentDate.format('YYYY-MM-DD')} 
-                onChange={handleDateChange('finishRentDate')} 
+                onChange={handleDateChange('finishRentDate')}
+                required 
             />
             <textarea
                 id='comments' 
                 name="comments" 
                 value={form.comments} 
                 onChange={handleChange} 
-                placeholder="Comments" />
-            <button 
-                id='submit' 
-                type="submit">
-                    Submit
-            </button>
-            <button 
-                id='button' 
-                onClick={handleReset} 
-                type="reset">
-                    Reset
-            </button>
+                placeholder="Comments" 
+            />
+            <div id='buttons'>
+                <button 
+                    id='submit' 
+                    type="submit">
+                        Submit
+                </button>
+                <button 
+                    id='reset' 
+                    onClick={handleReset} 
+                    type="reset">
+                        Reset
+                </button>
+            </div>
         </form>
     );
 };
