08e2bed5a01a85bd7ef307ee4d8b78dbfc50555f
Thundem <vovkandrij7@gmail.com>
2024-04-19 23:44:38 +0300

    fixed App component to include RentCarForm and FormResults components

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Form from './components/RentCarForm';
+import RentCarForm, { RentCar } from './components/RentCarForm';
+import FormResults from './components/FormResult';
+import dayjs from 'dayjs';
 
 function App() {
+  const initialFormState: RentCar = {
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    email: '',
+    placeOfIssue: '',
+    startRentDate: dayjs(),
+    finishRentDate: dayjs(),
+    comments: '',
+  };
+
+  const [form, setForm] = useState<RentCar>(initialFormState);
+  const [submittedForms, setSubmittedForms] = useState<RentCar[]>([]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmittedForms([...submittedForms, form]);
+    setForm(initialFormState);
+  };
+
   return (
     <div className="App">
-      <Form />
+      <div id='rent'>
+        <RentCarForm form={form} setForm={setForm} onSubmit={handleSubmit} />
+      </div>
+      <div id='result'>
+        {submittedForms.map((form, index) => (
+          <FormResults key={index} form={form} />
+        ))}
+      </div>
     </div>
   );
 }
