f854770d68c0eb1d741edbb285c9ad0de23e835a
Thundem <vovkandrij7@gmail.com>
2024-04-25 00:35:52 +0300

    rewrite form with component from mui

--- src/components/RentCarForm.tsx
+++ src/components/RentCarForm.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import dayjs from 'dayjs';
 import InputMask from 'react-input-mask';
-import styles from '../App.module.css'; 
 import { RentCar, initialFormState } from '../interfaces';
+import { TextField, Button, Box } from '@mui/material';
 
 interface RentCarFormProps {
     form: RentCar;
@@ -32,97 +32,203 @@ const RentCarForm: React.FC<RentCarFormProps> = ({ form, setForm, onSubmit}) => 
         handleDateChange('startRentDate')(e);
     };
 
+    const FormStyle = {
+        minWidth: '250px',
+        maxWidth: '300px',
+        marginLeft: '50px',
+        float: 'left',
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '70px',
+        backgroundColor: '#f8f9fa',
+        padding: '20px',
+        borderRadius: '10px',
+        boxShadow: '0 0 10px rgba(0,0,0,0.20)',
+    }
+
+    const FullNameStyle = {
+        width: '97%',
+        display: 'flex',
+        justifyContent: 'space-between',
+    }
+
+    const FirstAndLastNameStyle = {
+        width: 'calc(50% - 5px)',
+        padding: '10px',
+        borderRadius: '5px',
+        marginBottom: '10px',
+        fontSize: '16px',
+        color: '#333',
+        fontFamily: 'Comic Sans MS, sans-serif',
+    }
+
+    const TextFieldStyle = {
+        width: '90%',
+        padding: '10px',
+        borderRadius: '5px',
+        marginBottom: '10px',
+        fontSize: '16px',
+        color: '#333',
+        fontFamily: 'Comic Sans MS, sans-serif',
+    }
+
+    const SubmitButtonStyle = {
+        width: '44%',
+        padding: '5px',
+        marginTop: '10px',
+        border: 'none',
+        borderRadius: '5px',
+        color: '#ffffff',
+        backgroundColor: '#007bff',
+        marginRight: '10%', 
+        '&:hover': {
+            backgroundColor: '#003d7f',
+        },
+    }
+
+    const ResetButtonStyle = {
+        width: '44%',
+        padding: '5px',
+        marginTop: '10px',
+        border: 'none',
+        borderRadius: '5px',
+        color: '#ffffff',
+        backgroundColor: '#dc3545',
+        '&:hover': {
+            backgroundColor: '#9a000f',
+        },
+    }
+
     return (
-        <form onSubmit={(e) => { onSubmit(e); }} className={styles.rentCar}>
-            <div id={styles.fullName}>
-                <input 
-                    className={styles.firstName} 
+        <Box 
+            component="form" 
+            onSubmit={(e) => { onSubmit(e); }} 
+            sx={FormStyle}
+        >
+            <Box 
+                id="fullName" 
+                sx={FullNameStyle}
+            >
+                <TextField 
+                    className="firstName" 
+                    label="First Name"
                     name="firstName" 
                     value={form.firstName} 
                     onChange={handleChange} 
                     placeholder="First Name" 
                     required
-                    pattern="^[A-Z][a-z]*$"
+                    inputProps = {{ pattern: "^[A-Z][a-z]*$" }}
                     title="First name must start with a capital letter and contain only letters."
+                    sx={FirstAndLastNameStyle}
                 />
-                <input 
-                    className={styles.lastName} 
+                <TextField 
+                    className="lastName" 
+                    label="Last Name"
                     name="lastName" 
                     value={form.lastName} 
                     onChange={handleChange} 
                     placeholder="Last Name" 
                     required
-                    pattern="^[A-Z][a-z]*$"
+                    inputProps = {{ pattern: "^[A-Z][a-z]*$" }}
                     title="Last name must start with a capital letter and contain only letters."
+                    sx={FirstAndLastNameStyle}
                 />
-            </div>
-            <InputMask 
+            </Box>
+            <InputMask
                 mask="+38(099) 999 9999"
-                className={styles.phoneNumber} 
-                name="phoneNumber" 
-                value={form.phoneNumber} 
-                onChange={handleChange} 
-                placeholder="Phone Number"
-                required 
-            />
-            <input 
-                className={styles.email} 
+                value={form.phoneNumber}
+                onChange={handleChange}
+            >
+                {(inputProps) => (
+                    <TextField
+                        {...inputProps}
+                        name="phoneNumber"
+                        label="Phone Number"
+                        placeholder="+38(0__) ___ ____"
+                        required
+                        title="Please enter a valid phone number in the format: +38(0__) ___ ____"
+                        sx={TextFieldStyle}
+                    />
+                )}
+            </InputMask>
+            <TextField 
+                className="email"
+                label="Email" 
                 name="email" 
                 value={form.email} 
                 onChange={handleChange} 
                 placeholder="Email" 
                 required
-                pattern=".+@.+"
+                inputProps={{ pattern: ".+@.+" }}
                 title="Email must contain '@'."
+                sx={TextFieldStyle}
             />
-            <input 
-                className={styles.placeOfIssue} 
+            <TextField 
+                className="placeOfIssue"
+                label="Place of Issue"   
                 name="placeOfIssue" 
                 value={form.placeOfIssue} 
                 onChange={handleChange} 
                 placeholder="Place of Issue" 
                 required
-                pattern="^[A-Z][a-z]*$"
+                inputProps={{ pattern: "^[A-Z][a-z]*$" }}
+                sx={TextFieldStyle}
             />
-            <input 
+            <TextField 
                 type="date" 
-                className={styles.startRentDate} 
+                label="Start Rent Date"
+                id="outlined-password-input"
+                className="startRentDate" 
                 name="startRentDate" 
                 value={form.startRentDate.format('YYYY-MM-DD')} 
                 onChange={handleStartRentDateChange} 
-                min={new Date().toISOString().split('T')[0]}
+                inputProps={{ min: new Date().toISOString().split('T')[0] }}
                 required
+                sx={TextFieldStyle}
             />
-            <input 
+            <TextField 
                 type="date" 
-                className={styles.finishRentDate} 
+                label="Finish Rent Date"
+                className="finishRentDate" 
                 name="finishRentDate" 
                 value={form.finishRentDate.format('YYYY-MM-DD')} 
                 onChange={handleDateChange('finishRentDate')}
-                min={startRentDate}
                 required 
+                inputProps={{ min: startRentDate }}
+                sx={TextFieldStyle}
             />
-            <textarea
-                className={styles.comments}
+            <TextField
+                className="comments"
+                label="Comments"
                 name="comments" 
                 value={form.comments} 
                 onChange={handleChange} 
                 placeholder="Comments" 
+                sx={TextFieldStyle}
             />
-            <div className={styles.buttons}>
-                <button 
-                    className={styles.submit} 
-                    type="submit">
-                        Submit
-                </button>
-                <button 
-                    className={styles.reset} 
+            <Box 
+                className="buttons"
+                sx={{
+                    width: '100%',
+                }}
+            >
+                <Button 
+                    className="submit" 
+                    type="submit"
+                    sx={SubmitButtonStyle}
+                >
+                    Submit
+                </Button>
+                <Button 
+                    className="reset" 
                     onClick={handleReset} 
-                    type="reset">
-                        Reset
-                </button>
-            </div>
-        </form>
+                    type="reset"
+                    sx={ResetButtonStyle}
+                >
+                    Reset
+                </Button>
+            </Box>
+        </Box>
     );
 };
 
