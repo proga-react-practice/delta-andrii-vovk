90febd39afc4ef4bc4ed96b5768e32d2866b4f69
Thundem <vovkandrij7@gmail.com>
2024-04-28 22:25:24 +0300

    added palette property

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,6 +4,18 @@ const theme = createTheme({
   typography: {
     fontFamily: 'Comic Sans MS, sans-serif',
   },
+  palette: {
+    primary: {
+      main: '#525252',
+    },
+    secondary: {
+      main: '#525252',
+    },
+    background: {
+      default: '#f8f9fa',
+      paper: '#f8f9fa',
+    }
+  }
 });
 
 export default theme;
