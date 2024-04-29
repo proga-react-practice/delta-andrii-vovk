79f72dce88446489f27070ec24fcb045112dd70b
Thundem <vovkandrij7@gmail.com>
2024-04-29 14:46:46 +0300

    added some property to palette in theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -13,8 +13,22 @@ const theme = createTheme({
     },
     background: {
       default: '#f8f9fa',
-      paper: '#f8f9fa',
-    }
+    },
+    error: {
+      main: '#c84238',
+      light: '#dc3545',
+      dark: '#9a000f'
+    },
+    success: {
+      main: '#28a745',
+      light: '#34d058',
+      dark: '#22863a'
+    },
+    info: {
+      main: '#17a2b8',
+      light: '#007bff',
+      dark: '#003d7f'
+    },
   }
 });
 
