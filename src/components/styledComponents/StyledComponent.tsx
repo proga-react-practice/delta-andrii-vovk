9dac6c499066c0086ea4368fc894e547546c5799
Thundem <vovkandrij7@gmail.com>
2024-05-05 22:28:58 +0300

    fixed StyledTableRow component

--- src/components/styledComponents/StyledComponent.tsx
+++ src/components/styledComponents/StyledComponent.tsx
@@ -29,6 +29,19 @@ export const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:last-child td, &:last-child th': {
     border: 0,
   },
+  '@keyframes scaleDown': {
+    '0%': {
+      transform: 'scaleY(0)',
+      transformOrigin: 'top',
+      backgroundColor: 'transparent',
+    },
+    '100%': {
+      transform: 'scaleY(1)',
+      transformOrigin: 'top',
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+  animation: 'scaleDown 1s ease-in-out',
 }));
 
 export const StyledButton = styled(Button)(({theme}) => ({
